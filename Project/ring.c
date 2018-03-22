77965001b70f96178eacc7374855db3ef1a2b638
miguelpinho <33704675+miguelpinho@users.noreply.github.com>
2018-03-22 15:25:51 +0000

    First draft of ring maintance protocol. Started on availability but it might be better to postpone

--- Project/ring.c
+++ Project/ring.c
@@ -6,34 +6,33 @@
 struct state_fellow {
   int start;
   int available;
+  int ring_unavailable;
   int dispatch;
 }
 
-struct state_ring {
-  int available;
-}
-
-# next server in the fellowship ring
+/* next server in the fellowship ring */
 struct next_fellow {
   int id;
   char ip[MAX_STR];
   int tpt;
 };
 
+/*****STEP 2 BEGIN: ring maintenance*****/
 void enter_ring() {
   /* set next to start server */
 
   /* how does it communicate it is available??? send D anyway? */
 
   /* wait for connection? */
+
 }
 
 void new_arrival_ring() {
-  /* if (there is a next ) */
+  /* if (there is a next) */
     /* conects itself to the new fellow */
 
   /* else */
-    /* passes the  */
+    /* passes the token*/
 
     /* wait for desconnection from tail?? */
 
@@ -49,23 +48,62 @@ void token_new(int id, int id2, char *ip, int tpt) {
 }
 
 void exit_ring() {
+  /* if (start) */
+    /* withdraw start */
 
+    /* if (next is null) */
+      /* return (ring is over) */
+
+    /* else */
+      /* set next as start */
+
+  /* pass token exit */
+
+  /* wait for disconnect? */
 }
 
 void token_exit() {
   /* if (id == next) */
     /* if (id2 == self) */
+      /* disconnect from next */
       /* set next to null */
 
-    /* else */
+    /* else *
+      /* disconnect id */
       /* connect to id2 */
 
   /* else */
-    /* pass the token */
+    /* pass the E token */
 
 }
 
+void send_token() {
+
+}
+/*****STEP 2 END*****/
+
+/*****STEP 3 BEGIN: manage availability*****/
 void become_unavailable() {
+  /* if (is not dispatch)  ?????? */
+  /* if (is not available) ?????? */
+  /*  */
+
+  /* withdraw_ds from cs */
+
+  /* if (there is no next) */ /* this is the only fellow */
+    /* declare the ring recv_unavailable */
+
+
+  /* else */
+    /* creates and sends S token to next */
+    
+}
+
+void token_search() {
+  //
+}
+
+void token_transfer() {
 
 }
 
@@ -73,14 +111,7 @@ void become_available() {
 
 }
 
-void recv_unavailable() {
+void get_state() {
 
 }
-
-void recv_available() {
-
-}
-
-void state() {
-
-}
+/*****STEP 3 END*****/
