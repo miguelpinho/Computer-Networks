cbe48724f8504954b6d0cd0b2cea4203bbce227e
Malaca <miguelmmalaca@tecnico.ulisboa.pt>
2018-03-26 02:19:23 +0100

    Implemented everyting but the exit, with some things missing. A tosta mista to serve them all

--- Project/parse_test.c
+++ Project/parse_test.c
@@ -25,7 +25,7 @@ int id, tpt, id2;
   sscanf(buffer, "%s %s", msg_type, msg_data);
 
   printf("%s\n", msg_type);
-    printf("%s\n", msg_data);
+  printf("%s\n", msg_data);
 
   if (strcmp(msg_type, "TOKEN") == 0) {
     sscanf(msg_data, "%d;%c",&id, &token_type);
@@ -44,7 +44,7 @@ int id, tpt, id2;
         break;
     }
   } else if (strcmp(msg_type, "NEW")== 0) {
-    sscanf(msg_data, "%d;%[^; ];%d", &id, ip, &tpt);
+    sscanf(msg_data, "%d;%[^;];%d", &id, ip, &tpt);
 
   } else if (strcmp(msg_type, "NEW_START") == 0)   {
 
--- Project/ring.c
+++ Project/ring.c
@@ -72,15 +72,88 @@ void destroy_fellow(struct fellow *this) {
   close(this->fd_aux);
 }
 
-void send_token() {
+void send_token( char token_type, struct fellow, int id2, char *ip, int tpt, int id_start) {
 
+int n, nbytes, nleft, nwritten, ptr;
+char msg_out[MAX_STR], buffer[MAX_STR];
 
+  switch (token_type) {
+    case 'S': case 'T': case 'I': case 'D':
+      sprintf(msg_out, "TOKEN %d;%c\n", fellow.id, token_type);
+      break;
+    case 'N': case 'O':
+      sprintf(msg_out, "%d;%c;%d;%s;%d\n", id_start, token_type, id2, ip, tpt);
+      break;
+    default:
+      printf("Error: Token type not known\n");
+      return;
+      break;
+  }
+
+  /* addr_next.sin_family = AF_INET;
+  addr_next.sin_addr.s_addr = inet_addr(fellow.next.ip);
+  addr_next.sin_port = htons(fellow.next.tpt); */
+
+  /* n=connect(fellow.next.fd_next, (struct sockaddr*)&addr_next,sizeof(addr_next));
+	if(n==-1) exit(1); /* error */
+
+	ptr=strcpy(buffer, msg_out);
+	nbytes=strlen(msg_out);
+
+	nleft=nbytes;
+	while(nleft>0) {
+		nwritten=write(fellow.next.fd_next,ptr,nleft);
+		if(nwritten<=0) exit(1);//error
+
+		nleft-=nwritten;
+		ptr+=nwritten;
+	}
+
+	/* nleft=nbytes;
+	ptr=buffer;
+	while(nleft>0) {
+		nread=read(fellow.fd_prev,ptr,nleft);
+		if(nread==-1) exit(1); //error
+		else if(nread==0) break; //closed by peer
+		nleft-=nread;
+		ptr+=nread;
+	}
+	nread=nbytes-nleft; */
 }
 
 /*****STEP 2 BEGIN: ring maintenance*****/
 /* GET_start is not null */
-void join_ring() {
+void join_ring( struct fellow *fellow , int tpt_start , char *ip_start, int id_start) {
   /* connect to start server */
+
+  fellow->next.id = id_start;
+  fellow->next.tpt = tpt_start;
+  strcpy(fellow->next.ip, ip_start);
+
+  fellow->next.fd_next=socket(AF_INET,SOCK_STREAM,0); //TCP socket
+	if(fellow->next.fd_next==-1) exit(1);//error
+
+  addr_next.sin_family = AF_INET;
+  addr_next.sin_addr.s_addr = inet_addr(ip_start);
+  addr_next.sin_port = htons(tpt_start);
+
+  n=connect(fellow.next.fd_next, (struct sockaddr*)&addr_next,sizeof(addr_next));
+	if(n==-1) exit(1); /* error */
+
+  sprintf(msg_out, "NEW %d;%s;%d\n", fellow->id, fellow->ip, fellow->tpt);
+
+  ptr=strcpy(buffer, msg_out);
+	nbytes=strlen(msg_out);
+
+	nleft=nbytes;
+	while(nleft>0) {
+		nwritten=write(fellow.next.fd_next,ptr,nleft);
+		if(nwritten<=0) exit(1);//error
+
+		nleft-=nwritten;
+		ptr+=nwritten;
+	}
+
 
   /* wait for connection? */
   /* listen?? */
@@ -90,24 +163,69 @@ void join_ring() {
 }
 
 /* Start accepted new tcp */
-void new_arrival_ring() {
+void new_arrival_ring(struct fellow *fellow, int id_new, int tpt_new, char *ip_new, int id_start) {
   /* if (there is a next) */
     /* conects itself to the new fellow */
 
-  /* else */
-    /* passes the token*/
+  if(fellow->next.id == id_start) {
+
+
+    fellow->next.id = id_new;
+    fellow->next.tpt = tpt_new;
+    strcpy(fellow->next.ip, ip_new);
+
+    fellow->next.fd_next=socket(AF_INET,SOCK_STREAM,0); //TCP socket
+  	if(fellow->next.fd_next==-1) exit(1);/* error */
+
+    addr_new.sin_family = AF_INET;
+    addr_new.sin_addr.s_addr = inet_addr(ip_new);
+    addr_new.sin_port = htons(tpt);
+
+    n=connect(fellow->next.fd_next, (struct sockaddr*)&addr_new,sizeof(addr_new));
+    if(n==-1) exit(1); /* error */
+
+    /* desconnection from tail ???? TODO*/
+
+    /* else */
+      /* passes the token*/
+
+  } else {
+
+    send_token('N', *fellow, id_new, ip_new, tpt_new, id_start);
+
+  }
+
 
     /* wait for desconnection from tail?? */
 
 }
 
 /* Receives token new */
-void token_new(int id, int id2, char *ip, int tpt) {
+void token_new(struct fellow *fellow, int id2, char *ip, int tpt) {
   /* if (id == next) */
     /* connect to id2 */
+    if(fellow->next.id == -1) {
+      fellow->next.id = id_new;
+      fellow->next.tpt = tpt_new;
+      strcpy(fellow->next.ip, ip_new);
+
+      fellow->next.fd_next=socket(AF_INET,SOCK_STREAM,0); //TCP socket
+    	if(fellow->next.fd_next==-1) exit(1);/* error */
+
+      addr_new.sin_family = AF_INET;
+      addr_new.sin_addr.s_addr = inet_addr(ip);
+      addr_new.sin_port = htons(tpt);
+
+      n=connect(fellow->next.fd_next, (struct sockaddr*)&addr_new,sizeof(addr_new));
+    	if(n==-1) exit(1); /* error */
 
   /* else */
     /* pass token to next */
+
+    } else {
+      send_token('N', *fellow, id2, ip, tpt);
+    }
+
 
 }
 
